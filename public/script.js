0fa74f4d789c0429a7fa870f80010970098a02bc
Henry Egloff <henryegloff@users.noreply.github.com>
2023-10-12 21:38:05 +1100

    Adding web components

--- public/script.js
+++ public/script.js
@@ -1,2 +1,66 @@
 
-console.log("Hello JavaScript World!")
+console.log("Hello JavaScript World!");
+
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+     
+	    <header>
+	        <nav>
+	        	<ul>
+
+		            <li>
+		            	<a href="projects.html">
+		            		Projects
+		            	</a>
+		            </li>
+
+		            <li>
+		            	<a href="about.html">
+		            		Work
+		            	</a>
+		            </li>
+
+		            <li>
+		            	<a href="articles.html">
+		            		Articles
+		            	</a>
+		            </li>
+		            
+	        	</ul>
+	        </nav>
+	    </header>
+
+	    <main>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
+
+
+
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+
+     	</main>
+     	
+	    <footer>
+	       <p>&amp; Copyright</p>
+	    </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);
